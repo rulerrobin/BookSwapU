cc102508f0895a2431e0c74611452e4c70b4c05f
Genuel Mark <gmzapanta98@gmail.com>
2023-08-31 15:36:47 +1000

    names now appear in the my messages box for any received or current chats

--- src/BookSwapU/src/components/MyChats.jsx
+++ src/BookSwapU/src/components/MyChats.jsx
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from 'react'
 import { ChatState } from '../Context/ChatProvider'
 import axios from 'axios'
-import { useToast } from '@chakra-ui/react'
+import { Box, Button, Stack, Text, useToast } from '@chakra-ui/react'
+import ChatLoading from './ChatLoading'
+import getSender from './ChatLogic'
 
 
 const MyChats = () => {
@@ -19,6 +21,7 @@ const MyChats = () => {
         }
 
         const { data } = await axios.get('http://localhost:5000/api/chat', config)
+        console.log(data)
         setChats(data)
       } catch (error) {
         toast({
@@ -38,8 +41,64 @@ const MyChats = () => {
     }, [])
 
   return (
-    <div>MyChats</div>
+    <Box
+      display={{ base:selectedChat ? "none" : "flex", md: "flex" }}
+      flexDir="column"
+      alignItems="center"
+      p={3}
+      background="white"
+      width={{ base: "100", md: "31%"}}
+      borderRadius="lg"
+      borderWidth="1px"
+    >
+      <Box
+        pb={3}
+        px={3}
+        fontSize={{ base: "28px", md: "30px"}}
+        display="flex"
+        width="100"
+        justifyContent="space-between"
+        alignItems="center"
+      >
+        My Messages
+      </Box>
+      <Box
+        display="flex"
+        flexDir="column"
+        p={3}
+        bg="#F8F8F8"
+        width="100%"
+        height="100%"
+        borderRadius="lg"
+        overflowY="hidden"
+      >
+        {chats ? (
+          <Stack overflowY='scroll'>
+            {chats.map((chat) => (
+              <Box
+                onClick={() => setSelectedChat(chat)}
+                cursor="pointer"
+                bg={selectedChat === chat ? "#38B2AC" : "#E8E8E8"}
+                color={selectedChat === chat ? "white" : "black"}
+                px={3}
+                py={2}
+                borderRadius="lg"
+                key={chat._id}
+              >
+                
+                <Text>
+                    {chat.chatName = getSender(loggedUser, chat.users)}     
+                </Text>
+              </Box>
+            ))}
+          </Stack>
+        ) : (
+          <ChatLoading />
+        )}
+      </Box>
+    </Box>
   )
 }
+
 
 export default MyChats
