f12164468f53c423526060ca7faf4b2d76921023
Genuel Mark <gmzapanta98@gmail.com>
2023-09-02 10:53:55 +1000

    re added useLocation as it was deleted at some point

--- src/BookSwapU/src/Context/ChatProvider.jsx
+++ src/BookSwapU/src/Context/ChatProvider.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { createContext, useContext, useState, useEffect } from "react"
-import { useNavigate } from "react-router-dom"
+import { useNavigate, useLocation } from "react-router-dom"
 
 
 
